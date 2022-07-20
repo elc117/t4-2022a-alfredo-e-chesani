7358e47970cafcba108d6c8eb4e5a737472b264d
LChesani <92533013+LChesani@users.noreply.github.com>
2022-07-20 15:51:40 -0300

    Add files via upload

---
+++ core/src/com/mygdx/game/Entity/MainCharacter.java
@@ -0,0 +1,32 @@
+package com.mygdx.game.Entity;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.Input.Keys;
+import java.util.ArrayList;
+
+public class MainCharacter extends Entity{
+
+    public MainCharacter(){
+        this.sprite = new Texture("Colour1/NoOutline/120x80_PNGSheets/_CrouchTransition.png");
+        setXY(50, 300);
+        this.hitBox = new Rectangle(this.x, this.y, 100, 100);
+    }
+    public void update(SpriteBatch batch, ArrayList<Floor> rects){
+        batch.draw(sprite, this.x, this.y, 200, 200);
+        this.hitBox.x = this.x;
+        this.hitBox.y = this.y;
+        if(speed > -200){
+            speed-=1;
+        }
+        this.colider(rects);
+        this.y += speed;
+        if(Gdx.input.isKeyPressed(Keys.DPAD_LEFT)) 
+      		this.x -= 500 * Gdx.graphics.getDeltaTime();
+   		if(Gdx.input.isKeyPressed(Keys.DPAD_RIGHT)) 
+      		this.x += 500 * Gdx.graphics.getDeltaTime();
+   		if(Gdx.input.isKeyPressed(Keys.DPAD_UP))
+      		speed = 20;
+    }
+}
