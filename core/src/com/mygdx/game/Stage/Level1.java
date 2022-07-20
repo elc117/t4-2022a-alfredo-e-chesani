19007049f661bc01de63cc69f284a6126476146d
LChesani <92533013+LChesani@users.noreply.github.com>
2022-07-20 16:22:34 -0300

    Update Level1.java

--- core/src/com/mygdx/game/Stage/Level1.java
+++ core/src/com/mygdx/game/Stage/Level1.java
@@ -1,8 +1,10 @@
 package com.mygdx.game.Stage;
-
+import com.badlogic.gdx.graphics.Texture;
 public class Level1 extends Stage{
     public Level1(){
-        this.addFloor(0, 0, 200, 50);
-        this.addFloor(200, 200, 200, 50);
+        this.background = new Texture("background.png");
+        addFloor(0, 0, 200, 50);
+        addFloor(200, 200, 200, 50);
+        addFloor(400, 400, 200, 50);
     }
 }
