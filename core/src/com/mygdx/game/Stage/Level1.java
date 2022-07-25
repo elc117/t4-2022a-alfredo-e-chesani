271f01105e6ef44b657805d7dc7dd457a90bf5e7
LChesani <lhchesani@gmail.com>
2022-07-24 22:57:00 -0300

    parede pro personagem não cair no limbo

--- core/src/com/mygdx/game/Stage/Level1.java
+++ core/src/com/mygdx/game/Stage/Level1.java
@@ -7,6 +7,8 @@ public class Level1 extends Stage{
         this.background = new Texture("background.png");
         this.mc = mc;
         addFloor(0, 0, Gdx.graphics.getWidth(), 50);
+        addFloor(0, 0, 50, Gdx.graphics.getHeight());
+        addFloor(Gdx.graphics.getWidth() - 50, 0, 50, Gdx.graphics.getHeight());
         addFloor(200, 200, 200, 50);
         addFloor(400, 400, 200, 50);
         addEnemy(1000, 0);
