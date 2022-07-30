89308a6dee0f1ff7fa210352bb1a3a6892eb8299
GalileuGalilei <alfredocossetin@outlook.com>
2022-07-29 23:57:00 -0300

    conflito corrigido(agr de certeza)

--- core/src/com/mygdx/game/Entity/Entity.java
+++ core/src/com/mygdx/game/Entity/Entity.java
@@ -1,7 +1,9 @@
 package com.mygdx.game.Entity;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Rectangle;
-import java.util.ArrayList; 
+import java.util.ArrayList;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 
 public class Entity{
