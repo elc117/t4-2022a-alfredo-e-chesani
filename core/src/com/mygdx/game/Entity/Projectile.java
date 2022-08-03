4424e88b20c26ce896f89eb4df7d589a563f89c8
LChesani <92533013+LChesani@users.noreply.github.com>
2022-08-03 16:16:59 -0300

    Update Projectile.java

--- core/src/com/mygdx/game/Entity/Projectile.java
+++ core/src/com/mygdx/game/Entity/Projectile.java
@@ -4,15 +4,19 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 public class Projectile extends Entity{
-    public Projectile(float x, float y){
+    Vector2 direcao;
+    public Projectile(float x, float y, Vector2 direcao){
+        this.direcao = direcao;
         setXY(x, y);
         this.hitBox = new Rectangle(this.x, this.y, 50, 50);
-        
     }
 
     public void update(SpriteBatch batch, TextureRegion frame){
         batch.draw(frame, this.x, this.y, this.hitBox.width, this.hitBox.height);
+        this.x += direcao.nor().x * 700 * Gdx.graphics.getDeltaTime();
+        this.y += direcao.nor().y * 700 * Gdx.graphics.getDeltaTime();
         this.hitBox.x = this.x;
         this.hitBox.y = this.y;
     }
